4876ef697bf67fc658d9146e7e8a15373381e35f
salome-1 <43770872+salome-1@users.noreply.github.com>
2018-10-06 04:53:19 +0700

    Add files via upload

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -2,11 +2,34 @@
 
 @section('content')
 <div class="container">
+	<h1>Top Up</h1>
 
-    @if (session('msg'))
-      <div class="alert alert-succes">
-        <p> {{ session('msg')}}</p>
-      </div>
-    @endif
+	<div class="table-responsive">
+		<table class="table table-striped" border="solid">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">Tanggal Top Up</th>
+					<th scope="col">Nominal Top Up</th>
+					<th scope="col">Bukti Foto</th>
+					<th scope="col">Deposit</th>
+					<th scope="col">Kredit</th>
+					<th scope="col">Status</th>
+				</tr>
+			</thead>
+			
+			<tbody>
+				@foreach($transactions as $transaction)
+					<tr>
+						<td scope="col">{{$transaction->created_at}}</th>
+						<td scope="col">{{ $transaction->nominal }}</th>
+						<td><img src="{{asset('storage/proof_image/' . $transaction->transaction_image)}}" alt="" width="50"></td>
+						<td scope="col"> {{$transaction->deposit}} </td>
+						<td scope="col"> {{$transaction->credit}} </td>
+						<td scope="col"> {{$transaction->status}} </td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 </div>
 @endsection
