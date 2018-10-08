58c64b85ea023e60ef0e7b53e5aac6a5bb54ce7e
salome-1 <43770872+salome-1@users.noreply.github.com>
2018-10-09 04:07:36 +0700

    Add files via upload

--- resources/views/projects/single.blade.php
+++ resources/views/projects/single.blade.php
@@ -5,23 +5,23 @@
   <div class="jumbotron">
     <div class="row">
       <div class="col-sm">
-        <img src="{{asset('storage/project_image/' . $projects->project_image)}}" alt="" width="150">
-        <h1>{{$projects->title}}</h1>
-        <p>{{$projects->description}}</p>
-        <p>Project dimulai pada : {{$projects->opened_at}}</p>
-        <p>Project ditutup pada : {{$projects->closed_at}}</p>
-        <p>Dana yang dibutuhkan : {{$projects->project_price}}</p>
-        <p>Slot yang tersedia : {{$projects->slot}}</p>
-        <p>Jumlah uang per slot : {{$projects->slot_price}}</p>
+        <img src="{{asset('storage/project_image/' . $project->project_image)}}" alt="" width="150">
+        <h1>{{$project->title}}</h1>
+        <p>{{$project->description}}</p>
+        <p>Project dimulai pada : {{$project->opened_at}}</p>
+        <p>Project ditutup pada : {{$project->closed_at}}</p>
+        <p>Dana yang dibutuhkan : {{$project->project_price}}</p>
+        <p>Slot yang tersedia : {{$project->slot}}</p>
+        <p>Jumlah uang per slot : {{$project->slot_price}}</p>
         <!-- <p>dibuat oleh : {project-user-name}</p> -->
 
         <p><a href="/projects" class="btn btn-primary btg-lg">Kembali Ke List Project</a></p>
 
 
         {{-- admin --}}
-        @if ($projects->isOwner())
-          <p><a href="/projects/{{$projects->id}}/edit" class="btn btn-primary btg-lg">Edit Project</a></p>
-          <form method="POST" action="/projects/{{$projects->id}}">
+        @if ($project->isOwner())
+          <p><a href="/projects/{{$project->id}}/edit" class="btn btn-primary btg-lg">Edit Project</a></p>
+          <form method="POST" action="/projects/{{$project->id}}">
             {{ csrf_field() }}
             <input type="hidden" name="_method" value="DELETE">
             <button type="submit" class="btn btn-danger">
@@ -32,18 +32,20 @@
       </div>
 
       {{-- button for slots --}}
-      <div class="col sm">
-        <h1 class="text-center">Beli Slot</h1>
-        @foreach ($slots as $slot)
-          @if ($slot->status == 0)
-            <a href="#" class="btn btn-primary btg-lg" style="margin:25px">{{$slot->price}}</a>          
-          @else
-            <p class="btn btn-light" style="margin:25px">Dibeli</p>
-          @endif
-            
-        @endforeach
-      </div>
-
+      @if (Auth::user())
+        <div class="col sm">
+          <h1 class="text-center">Beli Slot</h1>
+          @foreach ($slots as $slot)
+            @if ($slot->status == 0)
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-slotid="{{$slot->id}}" data-userid="{{Auth::user()->id}}" style="margin:10px">{{$slot->price}}</button>
+              @include('slots.formBuySlot')
+            @else
+              <p class="btn btn-light">Telah Dibeli</p>
+            @endif
+          @endforeach
+        </div>
+      @endif
+      
     </div>
   </div>
 </div>
